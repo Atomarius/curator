199eb6959f5a77b85578f4442961eae4ee430883
ElSuperbeano <marius.schuette@gmail.com>
2016-04-05 00:25:46 +0200

    test(FieldProcessor): Added test to ensure non matching parts are untouched

--- tests/PhpChangelog/FieldProcessorTest.php
+++ tests/PhpChangelog/FieldProcessorTest.php
@@ -6,16 +6,24 @@ class FieldProcessorTest extends \PHPUnit_Framework_TestCase
 {
     /** @var FieldProcessor */
     private $fixture;
+
     protected function setUp()
     {
         $this->fixture = new FieldProcessor();
     }
 
-    public function testReplaceMatchesWithLink()
+    public function testReplacesMatches()
     {
         $value = 'PSP-1234,SHOP-1234';
         $expected = '[PSP-1234](http://jira.goodgamestudios.com/browse/PSP-1234)'
             . ',[SHOP-1234](http://jira.goodgamestudios.com/browse/SHOP-1234)';
+        $this->assertEquals($expected, $this->fixture->process($value));
+    }
+
+    public function testLeavesNonMatchingPartsUntouched()
+    {
+        $value = 'pre,PSP-1234,post';
+        $expected = 'pre,[PSP-1234](http://jira.goodgamestudios.com/browse/PSP-1234),post';
         $this->assertEquals($expected, $this->fixture->process($value));
     }
 }
