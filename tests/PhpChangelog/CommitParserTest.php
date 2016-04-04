adcefa9863022867be9fe8a2058f33513468400f
ElSuperbeano <marius.schuette@gmail.com>
2016-04-04 23:29:47 +0200

    test(CommitParser): Added unit tests
    Added tests for returning array on match and original value on mismatch

---
+++ tests/PhpChangelog/CommitParserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PhpChangelog;
+
+class CommitParserTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var CommitParser */
+    private $fixture;
+
+    public function setUp()
+    {
+        $options = ['pattern' => "/(?<type>\w+)\((?<scope>.+)\):\s(?<subject>.+)/"];
+        $this-> fixture = new CommitParser($options);
+    }
+
+    public function testParseReturnsArrayOnMatch()
+    {
+        $value = 'type(scope): subject';
+        $expected = ['type' => 'type', 'scope' => 'scope', 'subject' => 'subject'];
+        $this->assertEquals($expected, $this->fixture->parse($value));
+    }
+
+    public function testReturnsStringOnMismatch()
+    {
+        $value = 'message';
+        $this->assertEquals($value, $this->fixture->parse($value));
+    }
+}
