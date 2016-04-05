bc93159a89679c82b31df96ecadd1a58f5316d51
ElSuperbeano <marius.schuette@gmail.com>
2016-04-05 02:35:14 +0200

    feat(ContainerBuilder): Added static method to generate PHP-DI container

---
+++ src/PhpChangelog/ContainerBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PhpChangelog;
+
+class ContainerBuilder
+{
+    /**
+     * @param array $definitions
+     *
+     * @return \DI\Container
+     */
+    public static function build($definitions)
+    {
+        $builder = new \DI\ContainerBuilder();
+        $builder->useAutowiring(false);
+        $builder->useAnnotations(false);
+        $builder->addDefinitions($definitions);
+
+        return $builder->build();
+    }
+}
