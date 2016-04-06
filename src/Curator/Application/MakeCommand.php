bcd90789849bb8112562b74bb6e37ba231368448
Marius Schütte <mschuette@goodgamestudios.com>
2016-04-06 16:34:40 +0200

    fix(MakeCommand): Missing use statement

--- src/Curator/Application/MakeCommand.php
+++ src/Curator/Application/MakeCommand.php
@@ -5,6 +5,7 @@ namespace Curator\Application;
 use Curator\ChangelogWriter;
 use Interop\Container\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,10 +24,12 @@ class MakeCommand extends Command
     {
         $this
             ->setName('make')
-            ->setDescription('Make new changelog')            ->addArgument(
+            ->setDescription('Make new changelog')
+            ->addArgument(
                 'filename',
                 InputArgument::OPTIONAL,
-                'Name of outputfile'
+                'Name of outputfile',
+                'CHANGELOG_TMP.md'
             );
 //        ->addOption('from', null, InputOption::VALUE_REQUIRED)
 //        ->addOption('to', null, InputOption::VALUE_REQUIRED);
@@ -36,7 +39,7 @@ class MakeCommand extends Command
     {
         /** @var ChangelogWriter $changelogWriter */
         $changelogWriter = $this->container->get('ChangelogWriter');
-        $filename = $input->hasArgument('filename') ? $input->getArgument('filename') : 'CHANGELOG_TMP.md';
+        $filename = $input->getArgument('filename');
         $changelogWriter->write($filename);
         $output->writeln("Changelog generated {$filename}");
 
