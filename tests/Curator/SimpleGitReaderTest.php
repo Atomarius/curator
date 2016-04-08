121e6d40f34878eddc893579031d6a58a6f44706
ElSuperbeano <marius.schuette@gmail.com>
2016-04-08 19:12:34 +0200

    docs(curator): Added file comments with license infromation

--- tests/Curator/SimpleGitReaderTest.php
+++ tests/Curator/SimpleGitReaderTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of Curator.
+ *
+ * (c) Marius Schütte <marius.schuette@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Curator;
 
 use Mockery as M;
