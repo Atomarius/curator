5c164c59e6d055e736bfa71cca9318d231de7e16
Marius Schütte <mschuette@goodgamestudios.com>
2016-04-20 11:44:00 +0200

    misc(config/curator.php): Removed index/categories from default config

--- config/curator.php
+++ config/curator.php
@@ -28,13 +28,7 @@ return [
         'index'    => [
             'feat'     => 'New Features', // A new feature
             'fix'      => 'Bug Fixes', // A bug fix
-            'docs'     => 'Documentation', // Documentation only changes
-            'style'    => 'Style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-            'refactor' => 'Refactoring', // A code change that neither fixes a bug nor adds a feature
-            'perf'     => 'Performance Improvements', // A code change that improves performance
-            'test'     => 'Test', // Adding missing tests
-            'chore'    => 'Chore', // Changes to the build process or auxiliary tools and libraries such as documentation generation
-            'misc'     => 'Miscellaneous'
+            'misc'     => 'Miscellaneous' // I do not know ;)
         ],
     ],
 ];
